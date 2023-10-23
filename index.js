1e21982eb570701ed5ee06598f4b8896ea586e53
agarwal <contact.mehul2314@gmail.com>
2023-10-23 18:43:13 +0530

    make it better

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 // const mysql= require('mysql'); // Require the mysql package
 const { createPool } = require('mysql2'); // Use mysql2
 const app = express();
-const port = 3000;
+const port = process.env.PORT||3000;
 const cors = require('cors');
 
 // Use CORS middleware to enable cross-origin requests
@@ -11,10 +11,11 @@ app.use(express.json());
 
 // Create a MySQL database connection
 const pool = createPool({
-    host: 'localhost', // Replace with your database host
-    user: 'root', // Replace with your database user
-    password: '@Mehul7488130026', // Replace with your database password
-    database: 'nuego_db', // Replace with your database name
+    // host: 'localhost', // Replace with your database host
+    host:'us-cdbr-east-06.cleardb.net',
+    user: 'b3f07c73b6fcde', // Replace with your database user
+    password: 'c1e4a7a3', // Replace with your database password
+    database: 'heroku_cc9c31584bb6343', // Replace with your database name
     connectionLimit:10,
   });
 
@@ -40,6 +41,6 @@ app.get('/api/bus-schedules', (req, res) => {
     });
   });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
